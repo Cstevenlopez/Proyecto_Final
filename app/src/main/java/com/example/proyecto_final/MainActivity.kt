381e7eb514b3625b027007b12da5b6bc0b569616
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-11 14:14:44 -0600

    agrege mas iconos para el login

--- app/src/main/java/com/example/proyecto_final/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_final/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.proyecto_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun  lanzarSegundaPantalla(view: View) {
+        val intent = Intent(this,MyLoginScreen ::class.java)
+        startActivity(intent)
+    }
+
 }
