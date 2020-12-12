b21259ba9daff3c1c5d86b925537f2a7569af81a
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-12 14:47:26 -0600

    Termine el login

--- app/src/main/java/com/example/proyecto_final/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_final/MainActivity.kt
@@ -9,10 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun  lanzarSegundaPantalla(view: View) {
-        val intent = Intent(this,MyLoginScreen ::class.java)
-        startActivity(intent)
-    }
 
+
+    }
 }
--- app/src/main/java/com/example/proyecto_final/MyLoginScreen.kt
+++ app/src/main/java/com/example/proyecto_final/MyLoginScreen.kt
@@ -1,60 +1,81 @@
 package com.example.proyecto_final
-
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
 
-class MyLoginScreen : AppCompatActivity() {
+class MyLoginScreen  : AppCompatActivity() {
+
+    private val TAG = "MyLoginScreen"
+    //global variables
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var mProgressBar: ProgressDialog
+
+    //Creamos nuestra variable de autenticación firebase
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_login_screen)
-        //Setup
-        setup()
+        initialice()
     }
 
-        private fun setup() {
-            title = "Autenticacion"
-            signUpButton.setOnClickListener {
-                if (emailEditText.text.isNotEmpy() && passwordEditText.text.isNotEmpy()) {
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                        emailEditText.text.toString(),
-                        passwordEditText.texttoString()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            showHome(it.result?.user?.email?:"",ProviderType.BASIC)
-                        } else {
-                        showAlert()
-                        }
+    private fun initialice() {
+        emailEditText = findViewById(R.id.emailEditText)!!
+        passwordEditText = findViewById(R.id.passwordEditText)!!
+        mProgressBar = ProgressDialog(this)!!
+        mAuth = FirebaseAuth.getInstance()!!
+    }
+    private fun loginUser()
+    {
+        //Obtenemos usuario y contraseña
+        email = emailEditText.text.toString()
+        password = passwordEditText.text.toString()
+        //Verificamos que los campos no este vacios
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
 
+            //Mostramos el progressdialog
+            mProgressBar.setMessage("Registering User...")
+            mProgressBar.show()
+
+            //Iniciamos sesión con el método signIn y enviamos usuario y contraseña
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) {
+                    //Verificamos que la tarea se ejecutó correctamente
+                        task ->
+                    if (task.isSuccessful) {
+                        // Si se inició correctamente la sesión vamos a la vista Home de la aplicación
+                        goHome() // Creamos nuestro método en la parte de abajo
+                    } else {
+                        // sino le avisamos el usuairo que orcurrio un problema
+                        Toast.makeText(this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
                     }
                 }
-            }
-            logInButton.setOnClickListener {
-                if (emailEditText.text.isNotEmpy() && passwordEditText.text.isNotEmpy()) {
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(
-                        emailEditText.text.toString(),
-                        passwordEditText.texttoString()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            showHome(it.result?.user?.email?:"",ProviderType.BASIC)
-                        } else {
-                            showAlert()
-                        }
+        } else {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
         }
-private fun showAlert(){
-    val builder= AlertDialog.Builder(this)
-    builder.setTitle("Error")
-    builder.setMessage("Se ha produciodo un error autenticando al usuario")
-    builder.setPositiveButton("Aceptar",null)
-    val dialog: AlertDialog = builder.create()
-    dialog.show()
-}
-    private fun showHome(email:String,provider:ProviderType){
-    val homeItent = Itent(this,HomeActivity::class.java).apply{
-        putExtra("email",email)
-        putExtra("provider",provider.name)
     }
-        startActivity(homeItent)
+
+    private fun goHome() {
+        mProgressBar.hide()
+        //Nos vamos a Home
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
     }
+
+    fun login(view: View) {
+        loginUser()
+    }
+    fun register(view: View) {}
+    fun forgotPassword(view: View) {}
 }
