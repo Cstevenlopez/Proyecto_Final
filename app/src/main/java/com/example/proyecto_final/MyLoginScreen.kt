985b147bb39be3681a31dfd62f1f4eb1ec4f2cd0
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-12 10:52:48 -0600

    Agrege un activity la parte de codigo de usuario y verificacion

--- app/src/main/java/com/example/proyecto_final/MyLoginScreen.kt
+++ app/src/main/java/com/example/proyecto_final/MyLoginScreen.kt
@@ -2,10 +2,40 @@ package com.example.proyecto_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class MyLoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_login_screen)
+        //Setup
+        setup()
     }
+
+        private fun setup() {
+            title = "Autenticacion"
+            signUpButton.setOnClickListener {
+                if (emailEditText.text.isNotEmpy() && passwordEditText.text.isNotEmpy()) {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.texttoString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                        } else {
+                        showAlert()
+                        }
+
+                    }
+                }
+            }
+        }
+private fun showAlert(){
+    val builder= AlertDialog.Builder(this)
+    builder.setTitle("Error")
+    builder.setMessage("Se ha produciodo un error autenticando al usuario")
+    builder.setPositiveButton("Aceptar",null)
+    val dialog: AlertDialog = builder.create()
+    dialog.show()
+}
 }
