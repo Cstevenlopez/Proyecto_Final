fd628dd71fb7b534362971c0685612d5abe38377
TonyNavas <tonynavas777@gmail.com>
2020-12-13 20:17:38 -0600

    Agregue una funcion Select que nos permite obtener un elemento por su ID

--- app/src/main/java/com/example/proyecto_final/RegistroCRUD.kt
+++ app/src/main/java/com/example/proyecto_final/RegistroCRUD.kt
@@ -6,15 +6,14 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class RegistroCRUD (context: Context)
-{
-    private var helper:DataBaseHelper? = null
-    init
-    {
+class RegistroCRUD (context: Context) {
+    private var helper: DataBaseHelper? = null
+
+    init {
         helper = DataBaseHelper(context)
     }
-    fun  newRegistro(item:Registro)
-    {
+
+    fun newRegistro(item: Registro) {
         //Abrir la base de datos
         val db: SQLiteDatabase = helper?.writableDatabase!!
 
@@ -33,20 +32,20 @@ class RegistroCRUD (context: Context)
         val newRowId = db.insert(RegistroContrac.Companion.Entrada.NOMBRE_TABLA, null, values)
         db.close()
     }
-    fun getRegistros(): ArrayList<Registro>
-    {
-        val items:ArrayList<Registro> = ArrayList()
+
+    fun getRegistros(): ArrayList<Registro> {
+        val items: ArrayList<Registro> = ArrayList()
 
         //Abrir base de datos en modo lectura
         val db: SQLiteDatabase = helper?.readableDatabase!!
 
 
         // Especificar las columnas que se van a consultar
-        val columnas = arrayOf(RegistroContrac.Companion.Entrada.COLUMNA_ID,RegistroContrac.Companion.Entrada.COMLUMNA_COMUNIDAD, RegistroContrac.Companion.Entrada.COLUMNA_NOMBRE, RegistroContrac.Companion.Entrada.COLUMNA_ACTIVIDAD, RegistroContrac.Companion.Entrada.COLUMNA_TEMA, RegistroContrac.Companion.Entrada.COLUMNA_OBJETIVO, RegistroContrac.Companion.Entrada.COLUMNA_RESUMEN)
+        val columnas = arrayOf(RegistroContrac.Companion.Entrada.COLUMNA_ID, RegistroContrac.Companion.Entrada.COMLUMNA_COMUNIDAD, RegistroContrac.Companion.Entrada.COLUMNA_NOMBRE, RegistroContrac.Companion.Entrada.COLUMNA_ACTIVIDAD, RegistroContrac.Companion.Entrada.COLUMNA_TEMA, RegistroContrac.Companion.Entrada.COLUMNA_OBJETIVO, RegistroContrac.Companion.Entrada.COLUMNA_RESUMEN)
 
         //Crear un cursor para recorrer la tabla
 
-        val c:Cursor = db.query(
+        val c: Cursor = db.query(
                 RegistroContrac.Companion.Entrada.NOMBRE_TABLA,
                 columnas,
                 null,
@@ -55,7 +54,7 @@ class RegistroCRUD (context: Context)
                 null,
                 null
         )
-        while (c.moveToNext()){
+        while (c.moveToNext()) {
             items.add(Registro(
                     c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_ID)),
                     c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COMLUMNA_COMUNIDAD)),
@@ -70,5 +69,33 @@ class RegistroCRUD (context: Context)
         db.close()
 
         return items
+    }
+
+    fun getRegistro(Id: String): Registro {
+        var item: Registro? = null
+
+        val db: SQLiteDatabase = helper?.readableDatabase!!
+        val columnas = arrayOf(RegistroContrac.Companion.Entrada.COLUMNA_ID, RegistroContrac.Companion.Entrada.COMLUMNA_COMUNIDAD, RegistroContrac.Companion.Entrada.COLUMNA_NOMBRE, RegistroContrac.Companion.Entrada.COLUMNA_ACTIVIDAD, RegistroContrac.Companion.Entrada.COLUMNA_TEMA, RegistroContrac.Companion.Entrada.COLUMNA_OBJETIVO, RegistroContrac.Companion.Entrada.COLUMNA_RESUMEN)
+
+        val c: Cursor = db.query(
+                RegistroContrac.Companion.Entrada.NOMBRE_TABLA,
+                columnas,
+                "Id = ?",
+                arrayOf(Id),
+                null,
+                null,
+                null
+        )
+        while (c.moveToNext()) {
+            item = Registro(c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_ID)),
+                            c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COMLUMNA_COMUNIDAD)),
+                    c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_NOMBRE)),
+                    c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_ACTIVIDAD)),
+                    c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_TEMA)),
+                    c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_OBJETIVO)),
+                    c.getString(c.getColumnIndexOrThrow(RegistroContrac.Companion.Entrada.COLUMNA_RESUMEN)))
+        }
+        c.close()
+        return item!!;
     }
 }
