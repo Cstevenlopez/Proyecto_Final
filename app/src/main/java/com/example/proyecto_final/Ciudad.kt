d13af1c6e938c6084b8ac04c961bdaa636727429
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-13 21:01:08 -0600

    Cree la clase ciudad

---
+++ app/src/main/java/com/example/proyecto_final/Ciudad.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto_final
+
+class Ciudad(name:String, weather:List<Weather>, main:Main) {
+    var name:String = ""
+    var weather:List<Weather>? = null
+    var main:Main? = null
+    init {
+        this.name = name
+        this.weather = weather
+        this.main = main
+    }
+
+}
