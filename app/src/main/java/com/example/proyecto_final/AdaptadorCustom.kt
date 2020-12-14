f036d641d19d9a97a2393e8a16fb590d422c5c54
TonyNavas <tonynavas777@gmail.com>
2020-12-14 14:49:54 -0600

    ...

--- app/src/main/java/com/example/proyecto_final/AdaptadorCustom.kt
+++ app/src/main/java/com/example/proyecto_final/AdaptadorCustom.kt
@@ -1,37 +1,42 @@
-package com.example.proyecto_final
-
 import android.graphics.Color
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.internal.bind.ReflectiveTypeAdapterFactory
+import com.example.proyecto_final.ClickListener
+import com.example.proyecto_final.LongClickListener
+import com.example.proyecto_final.R
+import com.example.proyecto_final.Registro
 
 
-class AdaptadorCustom(items:ArrayList<Registro>, var listener:ClickListener, var longClickListener:LongClickListener): RecyclerView.Adapter<AdaptadorCustom.ViewHolder>() {
+class AdaptadorCustom(items:ArrayList<Registro>, var listener: ClickListener, var longClickListener: LongClickListener): RecyclerView.Adapter<AdaptadorCustom.ViewHolder>() {
 
-    var items:ArrayList<Registro>? = null
+
+    var items: ArrayList<Registro>? = null
     var multiSeleccion = false
 
     var itemsSeleccionados: ArrayList<Int>? = null
-    var viewHolder:ViewHolder? = null
+    var viewHolder: ViewHolder? = null
 
     init {
         this.items = items
         itemsSeleccionados = ArrayList()
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val vista = LayoutInflater.from(parent.context).inflate(R.layout.emplate_registro,parent,false)
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.emplate_registro, parent, false)
         viewHolder = ViewHolder(vista, listener, longClickListener)
 
         return viewHolder!!
     }
+
     override fun getItemCount(): Int {
         return items?.count()!!
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val item = items?.get(position)
         holder.Id?.text = item?.Id
         holder.Comunidad?.text = item?.Comunidad
@@ -41,57 +46,63 @@ class AdaptadorCustom(items:ArrayList<Registro>, var listener:ClickListener, var
         holder.Objetivos?.text = item?.Objetivo
         holder.Resumen?.text = item?.Resumen
 
-
-        if (itemsSeleccionados?.contains(position)!!){
+        if (itemsSeleccionados?.contains(position)!!) {
             holder.vista.setBackgroundColor(Color.LTGRAY)
-        }else{
+        } else {
             holder.vista.setBackgroundColor(Color.WHITE)
         }
     }
-    fun iniciarActionMode()
-    {
+
+    fun iniciarActionMode() {
         multiSeleccion = true
     }
-    fun destruirActionMode()
-    {
+
+    fun destruirActionMode() {
         multiSeleccion = false
+        itemsSeleccionados?.clear()
         notifyDataSetChanged()
     }
-    fun terminarActionMode()
-    {
-        //EliminarElementosSeleccionados
-        for (item in itemsSeleccionados!!){
+
+    fun terminarActionMode() {
+        // eliminar elementos seleccionados
+        for (item in itemsSeleccionados!!) {
             itemsSeleccionados?.remove(item)
         }
         multiSeleccion = false
         notifyDataSetChanged()
     }
-    fun seleccionarItem(index: Int)
-    {
-        if(multiSeleccion){
-            if (itemsSeleccionados?.contains(index)!!){
+
+    fun seleccionarItem(index: Int) {
+        if (multiSeleccion) {
+            if (itemsSeleccionados?.contains(index)!!) {
                 itemsSeleccionados?.remove(index)
-            }else{
+            } else {
                 itemsSeleccionados?.add(index)
             }
+
             notifyDataSetChanged()
         }
     }
-        fun obtenerNumeroElementosSeleccionados():Int{
-            return itemsSeleccionados?.count()!!
-        }
-        fun eliminarSeleccionados(){
-            if(itemsSeleccionados?.count()!!>0){
-                var itemsEliminados = ArrayList<Registro>()
 
-                for (index in itemsSeleccionados!!){
-                    itemsEliminados.add(items?.get(index)!!)
-                }
-                items?.removeAll(itemsEliminados)
-                itemsSeleccionados?.clear()
+    fun obtenerNumeroElementosSeleccionados(): Int {
+        return itemsSeleccionados?.count()!!
+    }
+
+    fun eliminarSeleccionados() {
+        if (itemsSeleccionados?.count()!! > 0) {
+            var itemsEliminados = ArrayList<Registro>()
+
+            for (index in itemsSeleccionados!!) {
+                itemsEliminados.add(items?.get(index)!!)
             }
+
+            items?.removeAll(itemsEliminados)
+            itemsSeleccionados?.clear()
         }
-    class ViewHolder(vista: View, listener: ClickListener, longClickListener: LongClickListener): RecyclerView.Adapter<AdaptadorCustom.ViewHolder>(){
+    }
+
+
+    class ViewHolder(vista: View, listener: ClickListener, longClickListener: LongClickListener) : RecyclerView.ViewHolder(vista), View.OnClickListener, View.OnLongClickListener {
 
         var vista = vista
         var Id: TextView? = null
@@ -101,10 +112,11 @@ class AdaptadorCustom(items:ArrayList<Registro>, var listener:ClickListener, var
         var Temas: TextView? = null
         var Objetivos: TextView? = null
         var Resumen: TextView? = null
-        var listener:ClickListener? = null
+        var listener: ClickListener? = null
         var longListener: LongClickListener? = null
 
         init {
+
             Id = vista.findViewById(R.id.tvIdtem)
             Comunidad = vista.findViewById(R.id.tvComunidadtem)
             Nombre = vista.findViewById(R.id.tvNombretemp)
@@ -113,20 +125,24 @@ class AdaptadorCustom(items:ArrayList<Registro>, var listener:ClickListener, var
             Objetivos = vista.findViewById(R.id.tvObjetivotemp)
             Resumen = vista.findViewById(R.id.tvResumentemp)
 
+
+
             this.listener = listener
             this.longListener = longClickListener
 
             vista.setOnClickListener(this)
             vista.setOnLongClickListener(this)
         }
-        override fun onClick(v: View?){
-            this.listener?.onClick(v!!, adapterPositio)
+
+        override fun onClick(v: View?) {
+            this.listener?.onClick(v!!, adapterPosition)
         }
-        override fun onLongClick(v: View?): Boolean{
+
+        override fun onLongClick(v: View?): Boolean {
             this.longListener?.longClick(v!!, adapterPosition)
             return true
         }
+
+
     }
-
-
 }
