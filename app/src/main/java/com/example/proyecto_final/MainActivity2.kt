5280305a405553d613c7da66c539d391e7ed71c9
TonyNavas <tonynavas777@gmail.com>
2020-12-14 15:36:00 -0600

    ....

--- app/src/main/java/com/example/proyecto_final/MainActivity2.kt
+++ app/src/main/java/com/example/proyecto_final/MainActivity2.kt
@@ -1,11 +1,34 @@
 package com.example.proyecto_final
 
+import AdaptadorCustom
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity2 : AppCompatActivity() {
+
+    var lista: RecyclerView? = null
+    var adaptador: AdaptadorCustom? = null
+    var layoutManager: RecyclerView.LayoutManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab2)
+        lista = findViewById(R.id.Lista2)
+
+        lista?.setHasFixedSize(true)
+
+        layoutManager = LinearLayoutManager(this)
+        lista?.layoutManager = layoutManager
+
+        fab.setOnClickListener {
+            startActivity(Intent(this, DetallesRegistro::class.java))
+        }
     }
 }
