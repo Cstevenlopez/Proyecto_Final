b7f7039912b0cc188c5a7822745a0e823089506c
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-13 19:58:39 -0600

    Cree el splash screen

--- app/src/main/java/com/example/proyecto_final/MySplashScreen.kt
+++ app/src/main/java/com/example/proyecto_final/MySplashScreen.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MySplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_splash_screen)
     }
