b0c1adff289ac29ad73c7df99b1cff7220dbdcfd
TonyNavas <tonynavas777@gmail.com>
2020-12-13 18:43:40 -0600

    Agregue el contract que es donde se hace el esquema de las columnas que contendra la tabla

--- app/src/main/java/com/example/proyecto_final/RegistroContrac.kt
+++ app/src/main/java/com/example/proyecto_final/RegistroContrac.kt
@@ -1,4 +1,26 @@
 package com.example.proyecto_final
 
+import android.provider.BaseColumns
+
 class RegistroContrac {
+
+    companion object
+    {
+        val VERSION = 1
+        class Entrada: BaseColumns
+        {
+            companion object
+            {
+                val NOMBRE_TABLA = "RegistrarDatos"
+
+                val COLUMNA_ID = "Id"
+                val COMLUMNA_COMUNIDAD = "Comunidad"
+                val COLUMNA_NOMBRE = "Nombre"
+                val COLUMNA_ACTIVIDAD = "Actividad"
+                val COLUMNA_TEMA = "Tema"
+                val COLUMNA_OBJETIVO = "Objetivo"
+                val COLUMNA_RESUMEN = "Resumen"
+            }
+        }
+    }
 }
