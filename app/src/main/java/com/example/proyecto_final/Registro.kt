e45da075870d909e02106cbaab0de3f242711103
TonyNavas <tonynavas777@gmail.com>
2020-12-13 18:40:11 -0600

    Agregue la clase registro para declarar los datos

--- app/src/main/java/com/example/proyecto_final/Registro.kt
+++ app/src/main/java/com/example/proyecto_final/Registro.kt
@@ -1,4 +1,21 @@
 package com.example.proyecto_final
 
 class Registro {
+    var Id:String? = null;
+    var Comunidad:String? = null;
+    var  Nombre:String? = null;
+    var Actividad:String? = null;
+    var Tema:String? = null;
+    var Objetivo:String? = null;
+    var Resumen:String? = null;
+
+    init {
+        this.Id = Id;
+        this.Comunidad = Comunidad;
+        this.Nombre = Nombre;
+        this.Actividad = Actividad;
+        this.Tema = Tema;
+        this.Objetivo = Objetivo;
+        this.Resumen = Resumen;
+    }
 }
